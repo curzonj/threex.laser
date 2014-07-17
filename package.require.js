42e2bcd295bd2e872792f01c8d4ea0f8f892bbbc
Jordan Curzon <curzonj@gmail.com>
2014-07-17 05:13:10 -0700

    fix require.js file

--- package.require.js
+++ package.require.js
@@ -1,5 +1,6 @@
-define( [ './threex.laserbeam'
-	, './threex.lasercooked'
+define( [ 'module',
+          './threex.laserbeam',
+	  './threex.lasercooked'
 	], function(module){
 	// set baseUrl for this plugin
 	THREEx.LaserCooked.baseURL	= module.uri+'/../';
